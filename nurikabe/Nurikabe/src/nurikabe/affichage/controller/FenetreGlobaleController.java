8f68e3a3f520b85ae2f033542b4428e1758a76e2
HugFournier <33453923+HugFournier@users.noreply.github.com>
2017-12-02 18:57:12 +0100

    fix dernier ajout

--- nurikabe/Nurikabe/src/nurikabe/affichage/controller/FenetreGlobaleController.java
+++ nurikabe/Nurikabe/src/nurikabe/affichage/controller/FenetreGlobaleController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nurikabe.affichage.ihm;
+package nurikabe.affichage.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
